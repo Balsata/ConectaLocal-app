8b28c496017fc67e2376c88de9261ca0fd6f8711
Bryan <bryanalexst@hotmail.com>
2023-07-30 14:38:56 -0600

    Nueva db y plantilla de solicitud creada

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'Proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Open311",
+        "USER": "postgres",
+        "PASSWORD": "administrador",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
